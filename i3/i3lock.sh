5bdeb23fdd3eaa625ed830f10da759f7dfc96111
Justin Kenyon <kenyonj@gmail.com>
2015-03-31 09:27:22 -0400

    Update blurriness in lock file

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 4x4 /tmp/screen_locked2.png
+convert /tmp/screen_locked.png -blur 14x24 /tmp/screen_locked2.png
 i3lock -i /tmp/screen_locked2.png
