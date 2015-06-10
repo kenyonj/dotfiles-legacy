7a3babf6ba797fffc3d1dabacbdc01e7256199da
Justin Kenyon <kenyonj@gmail.com>
2015-06-10 13:38:34 -0400

    Updates June 2015

--- screenlayout/no-external.sh
+++ screenlayout/no-external.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1600x900 --pos 192x132 --rotate normal --output VGA1 --off
+xrandr --output VIRTUAL1 --off --output eDP1 --mode 1920x1200 --pos 2624x328 --rotate normal --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output VGA1 --off --output DP2 --off
