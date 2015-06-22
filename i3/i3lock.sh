71fc3caf3f5e2faed42ff12b79986466c9d2c611
Justin Kenyon <kenyonj@gmail.com>
2015-06-22 11:17:37 -0400

    Update blur method for lock screen
    Changes to a pixelated version.

--- i3/i3lock.sh
+++ i3/i3lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 scrot /tmp/screen_locked.png
-convert /tmp/screen_locked.png -blur 14x24 /tmp/screen_locked2.png
+convert -scale 5% -scale 2000% /tmp/screen_locked.png /tmp/screen_locked2.png
 i3lock -i /tmp/screen_locked2.png
