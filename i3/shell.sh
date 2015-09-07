3da19b451602e25020a6ad474267ecdf51346590
Justin Kenyon <kenyonj@gmail.com>
2015-09-06 20:45:32 -0400

    s/uxrvt/termite

--- i3/shell.sh
+++ i3/shell.sh
@@ -5,7 +5,7 @@ PID=$(($(xprop -id $ID | grep -m 1 PID | cut -d " " -f 3) + 2))
 
 if [ -e "/proc/$PID/cwd" ]
   then
-    urxvt -cd $(readlink /proc/$PID/cwd) &
+    termite -cd $(readlink /proc/$PID/cwd) &
   else
-    urxvt
+    termite
 fi
