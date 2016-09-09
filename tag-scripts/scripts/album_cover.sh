ce980baef387dba9f1105e537d4300fa725523a6
Justin Kenyon <kenyonj@gmail.com>
2016-09-09 16:43:23 -0400

    Update album cover pulling script

--- tag-scripts/scripts/album_cover.sh
+++ tag-scripts/scripts/album_cover.sh
@@ -1,34 +1,70 @@
 #!/bin/zsh
 
-no_album_art_found_image="$HOME/.scripts/notfound.jpg"
-current_song_artist=$(mpc current -f %artist% | sed 's/[ ,;]/+/g')
-current_song_album=$(mpc current -f %album% | sed 's/[ ,;]/+/g')
-current_song_title=$(mpc current -f %title% | sed 's/[ ,;]/+/g')
-current_file=$(mpc current -f %file%)
-array=("${(@s/:/)current_file}")
-song_source=$array[1]
-song_id=$array[3]
-itunes_api_url="https://itunes.apple.com/search?term=$current_song_artist+$current_song_album&country=US"
-alt_itunes_api_url="https://itunes.apple.com/search?term=$current_song_artist+$current_song_title&country=US"
-spotify_api_url="https://api.spotify.com/v1/tracks/$song_id"
+ITUNES_BASE_URL="https://itunes.apple.com/search?country=US&term="
+SPOTIFY_BASE_URL="https://api.spotify.com/v1/tracks/"
+NO_ALBUM_ART_FOUND_IMAGE="$HOME/.scripts/notfound.jpg"
 
-if [[ "$song_source" == "spotify" ]]; then
-  image_url=$(curl -X GET "$spotify_api_url" | ~/.local/bin/jsawk 'return this.album.images[0].url')
-  curl "$image_url" -o /tmp/cover.jpg
-else
-  result_count=$(curl -X GET "$itunes_api_url" | ~/.local/bin/jsawk 'return this.resultCount')
+api_url_for() {
+  if [[ "$1" == "spotify" ]]; then
+    echo "$SPOTIFY_BASE_URL`current_song_detail id`"
+  elif [[ "$1" == "itunes" ]]; then
+    echo "$ITUNES_BASE_URL$(current_song_detail artist)+$(current_song_detail album)"
+  elif [[ "$1" == "itunes-alt" ]]; then
+    echo "$ITUNES_BASE_URL$(current_song_detail artist)+$(current_song_detail title)"
+  fi
+}
 
-  if [[ "$result_count" > 0 ]]; then
-    image_url=$(curl -X GET "$itunes_api_url" | ~/.local/bin/jsawk 'return this.results[0].artworkUrl100')
-    curl "$image_url" -o /tmp/cover.jpg
+current_song_detail() {
+  if [[ "$1" == "file" ]]; then
+    mpc current -f %file%
+  elif [[ "$1" == "source" || "$1" == "id" ]]; then
+    local current_file=$(mpc current -f %file%)
+    local split_current_file=("${(@s/:/)current_file}")
+
+    if [[ "$1" == "source" ]]; then
+      echo $split_current_file[1]
+    elif [[ "$1" == "id" ]]; then
+      echo $split_current_file[3]
+    fi
   else
-    result_count=$(curl -X GET "$alt_itunes_api_url" | ~/.local/bin/jsawk 'return this.resultCount')
+    mpc current -f %$1% | sed 's/[ ,;]/+/g'
+  fi
+}
 
-    if [[ "$result_count" > 0 ]]; then
-      image_url=$(curl -X GET "$alt_itunes_api_url" | ~/.local/bin/jsawk 'return this.results[0].artworkUrl100')
-      curl "$image_url" -o /tmp/cover.jpg
+find_cover_image() {
+  if [[ "`current_song_detail source`" == "spotify" ]]; then
+    echo "$(get_image_url_from spotify)"
+  else
+    if [[ "`result_count_from itunes`" > 0 ]]; then
+      echo "$(get_image_url_from itunes)"
     else
-      cp $no_album_art_found_image /tmp/cover.jpg
+      if [[ "`result_count_from itunes-alt`" > 0 ]]; then
+        echo "$(get_image_url_from itunes-alt)"
+      else
+        echo "none found"
+      fi
     fi
   fi
-fi
+}
+
+get_image_url_from() {
+  if [[ "$1" == "spotify" ]]; then
+    curl -X GET $(api_url_for spotify) | ~/.local/bin/jsawk 'return this.album.images[0].url'
+  else
+    curl -X GET $(api_url_for $1) | ~/.local/bin/jsawk 'return this.results[0].artworkUrl100'
+  fi
+}
+
+result_count_from() {
+  echo "`curl -X GET $(api_url_for $1) | ~/.local/bin/jsawk 'return this.resultCount'`"
+}
+
+set_cover_image() {
+  if [[ "$1" == "none found" ]]; then
+    cp $NO_ALBUM_ART_FOUND_IMAGE /tmp/cover.jpg
+  else
+    curl $1 -o /tmp/cover.jpg
+  fi
+}
+
+set_cover_image "`find_cover_image`"
