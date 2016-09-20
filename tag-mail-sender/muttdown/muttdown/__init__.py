93cb160d38b9de0e8f6bd9b00e6727a31323112d
Justin Kenyon <kenyonj@gmail.com>
2016-09-20 16:20:31 -0400

    Remove submodules in favor of custom bin/setup

--- tag-mail-sender/muttdown/muttdown/__init__.py
+++
@@ -1,3 +0,0 @@
-version_info = (0, 2)
-__version__ = '.'.join(map(str, version_info))
-__author__ = 'James Brown <Roguelazer@gmail.com>'
--- tag-mail-sender/muttdown/muttdown/debug.py
+++
@@ -1,5 +0,0 @@
-import sys
-import email.iterators
-
-
-email.iterators._structure(email.message_from_file(sys.stdin))
