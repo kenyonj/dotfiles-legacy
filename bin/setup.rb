531c35b81cf780eab9aa1c1038e6593296c565f1
Justin Kenyon <kenyonj@gmail.com>
2016-12-22 13:45:05 -0500

    Add logic to modify gitignore and rework bin/setup

--- bin/setup.rb
+++ bin/setup.rb
@@ -23,9 +23,8 @@ class Configuration
       "repo_location" => "rawdigits/wee-slack.git",
       "tag" => "tag-weechat",
       "copy" => {
-        "path" => "tag-weechat/wee-slack/",
         "filename" => "wee_slack.py",
-        "destination_path" => "tag-weechat/weechat/python/autoload/",
+        "destination_path" => "tag-weechat/weechat/python/autoload",
       },
     },
     "zsh-syntax-highlighting" => {
@@ -52,15 +51,37 @@ class Configuration
       repo_location = details.dig("repo_location")
       tag = details.dig("tag")
       copy_details = details.dig("copy")
-      dependency_folder_location = [tag, base_path, name].compact.join("/")
+      dependency_folder_location =
+        [dotfiles_root, tag, base_path, name].compact.join("/")
 
       unless folder_exists?(dependency_folder_location)
         clone_repo(repo_location, dependency_folder_location)
       end
 
       update(dependency_folder_location)
-      copy_files(copy_details)
+      copy_files(dependency_folder_location, copy_details)
+      append_entry_to_gitignore(dependency_folder_location)
     end
+  end
+
+  def append_entry_to_gitignore(entry)
+    unless gitignore_entries.any? { |line| line[relative_path_for(entry)] }
+      File.open(gitignore_file, "a") do |file|
+        file.puts relative_path_for(entry)
+      end
+    end
+  end
+
+  def gitignore_entries
+    File.foreach(gitignore_file)
+  end
+
+  def gitignore_file
+    [dotfiles_root, ".gitignore"].join("/")
+  end
+
+  def relative_path_for(entry)
+    entry.gsub("#{dotfiles_root}/", "")
   end
 
   def folder_exists?(folder)
@@ -71,10 +92,16 @@ class Configuration
     system("git clone #{GIT_PREFIX}#{repo} #{folder}")
   end
 
-  def copy_files(details)
-    details &&
-      system(make_directory_command(details)) &&
-      system(copy_command(details))
+  def copy_files(dependency_folder_location, copy_details)
+    copy_details &&
+      system(make_directory_command(dependency_folder_location)) &&
+      system(copy_command(dependency_folder_location, copy_details)) &&
+      append_entry_to_gitignore(absolute_path_to_destination_file(copy_details))
+  end
+
+  def absolute_path_to_destination_file(copy_details)
+    [dotfiles_root, copy_details["destination_path"], copy_details["filename"]].
+      join("/")
   end
 
   def update(folder)
@@ -82,7 +109,7 @@ class Configuration
   end
 
   def rcup
-    system("export RCRC='#{home_path}/dotfiles/host-#{hostname}/rcrc'")
+    system("export RCRC='#{dotfiles_root}/host-#{hostname}/rcrc'")
     system("rcup")
   end
 
@@ -90,16 +117,22 @@ class Configuration
     ENV.fetch("HOME")
   end
 
+  def dotfiles_root
+    [home_path, "dotfiles"].join("/").freeze
+  end
+
   def hostname
     `hostname`.chomp
   end
 
-  def copy_command(details)
-    "cp #{details['path']}#{details['filename']} #{details['destination_path']}"
+  def copy_command(dependency_folder_location, copy_details)
+    "cp "\
+      "#{dependency_folder_location}/#{copy_details['filename']} "\
+      "#{dotfiles_root}/#{copy_details['destination_path']}/"
   end
 
-  def make_directory_command(details)
-    "mkdir -p #{details['destination_path']}"
+  def make_directory_command(dependency_folder_location)
+    "mkdir -p #{dependency_folder_location}"
   end
 end
 
