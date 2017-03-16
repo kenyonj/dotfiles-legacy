ef320cad4b8055237a44791a57fed5e6b42079b9
Justin Kenyon <kenyonj@gmail.com>
2017-03-15 21:34:43 -0400

    more updates

--- tag-i3/screenlayout/external-dp-left-of-open-laptop.sh
+++ tag-i3/screenlayout/external-dp-left-of-open-laptop.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 2560x512 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2-2-1 --off --output DP2-2-8 --primary --mode 2560x1440 --pos 0x0 --rotate normal --output DP2 --off
-xrandr --output DP2-2-8 --primary
-xrandr --output eDP1 --mode 2560x1440 --scale 1x1
+xrandr --output HDMI-1 --mode None --pos 1920x0 --rotate normal --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x752 --rotate normal --output DP-2 --off
--- tag-i3/screenlayout/no-external.sh
+++ tag-i3/screenlayout/no-external.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-xrandr --output VIRTUAL1 --off --output eDP1 --mode 2560x1440 --pos 0x0 --rotate normal --output DP1 --off --output DP2-1 --off --output DP2-2 --off --output DP2-3 --off --output HDMI2 --off --output HDMI1 --off --output DP2-2-1 --off --output DP2-2-8 --off --output DP2 --off
-xrandr --output eDP1 --primary
-xrandr --output eDP1 --mode 1920x1080 --scale 1x1
+xrandr --output HDMI-1 --off --output DP-1 --off --output eDP-1 --mode 1920x1080 --pos 0x0 --rotate normal --output DP-2 --off
